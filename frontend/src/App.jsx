2c576175988ad142f08031afb4e376bf2a5511df
Pedro Nogueira <pedronogueiramoraes@hotmail.com>
2026-04-20 11:40:09 -0300

    First components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import MovieCard from './components/MovieCard'
+
+function App() {
+
+  return (
+    <>
+      <MovieCard movie={{
+        title: "The Substence",
+        release_date: "2024"
+      }}/>
+    </>
+  )
+}
+
+
+
+export default App
